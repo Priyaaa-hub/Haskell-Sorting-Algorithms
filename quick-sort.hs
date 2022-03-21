8c65953590938e2efdef6980b806fccd9529e78f
Priyaaa-hub <78304406+Priyaaa-hub@users.noreply.github.com>
2022-03-21 09:17:28 +0530

    Add files via upload
    It uses divide and conquers techniques to perform sorting operations. Choose a pivot element and the elements smaller than it is placed on the left side of the array and elements greater than pivot are placed on the right side of the array.

---
+++ quick-sort.hs
@@ -0,0 +1,27 @@
+import Data.List (permutations)
+
+-- Return the list which contains elements less than or equal to the input element
+smallerEq :: Int -> [Int] -> [Int]
+smallerEq = \v -> \list ->
+    case list of
+        [] -> []
+        x:xs | x<=v -> x:smallerEq v xs
+        _:xs -> smallerEq v xs
+-- Return the list which contains elements greater than the specified element
+greater :: Int -> [Int] -> [Int]
+greater = \v -> \list ->
+    case list of
+        [] -> []
+        x:xs | x>v -> x:greater v xs
+        _:xs -> greater v xs
+
+-- Sort a list using the quicksort algorithm 
+qsort :: [Int] -> [Int]
+qsort = \list ->
+    case list of
+        [] -> []
+        x:xs -> 
+            qsort (smallerEq x xs) ++ x:qsort (greater x xs)
+
+
+main = print (qsort[8, 9, 3, 4, 7])
