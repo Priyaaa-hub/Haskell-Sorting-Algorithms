ac9dbc48eb166be481965abb66036093df02ba37
Priyaaa-hub <78304406+Priyaaa-hub@users.noreply.github.com>
2022-04-10 21:07:25 +0530

    Update merge-sort.hs

--- merge-sort.hs
+++ merge-sort.hs
@@ -1,12 +1,16 @@
-mergesort'merge :: (Ord a) => [a] -> [a] -> [a]
-mergesort'merge [] xs = xs
+--Merge Sort is a complicated sort--
+
+--The sorted parts are merged by a special merging procedure--
+mergesort'merge :: (Ord a) => [a] -> [a] -> [a]      --Receive two arrays and produce one array-- 
+mergesort'merge [] xs = xs                           --Return if any one of the list is empty--
 mergesort'merge xs [] = xs
-mergesort'merge (x:xs) (y:ys)
+mergesort'merge (x:xs) (y:ys)                        --Compare the first element and append--
     | (x < y) = x:mergesort'merge xs (y:ys)
     | otherwise = y:mergesort'merge (x:xs) ys
  
+ --List is split into two parts--
 mergesort'splitinhalf :: [a] -> ([a], [a])
-mergesort'splitinhalf xs = (take n xs, drop n xs)
+mergesort'splitinhalf xs = (take n xs, drop n xs)    --n is equeal to the half of length of array--
     where n = (length xs) `div` 2 
  
 mergesort :: (Ord a) => [a] -> [a]
